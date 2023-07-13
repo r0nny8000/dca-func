8a7f89a3573a345829d91eadb0018b14340374b0
r0nny8000 <r0nny8000@bea2b.de>
2023-07-13 22:41:25 +0200

    added linter

--- function_app.py
+++ function_app.py
@@ -1,9 +1,11 @@
-import azure.functions as func
 import logging
+import azure.functions as func
 
 app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("test function hello executed")
     return func.HttpResponse("HttpTrigger1 function processed a request!")
+    
