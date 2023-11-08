028566fb0ff57f24e91a0ea6d49b1808fac62c00
r0nny8000 <r0nny8000@bea2b.de>
2023-11-08 21:37:21 +0100

    Using github actions now

--- function_app.py
+++ function_app.py
@@ -6,6 +6,6 @@ app = func.FunctionApp()
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("TEST FUNCTION EXECUTED: %s", req.method)
-    return func.HttpResponse("HttpTrigger1 function processed a request!")
+    logging.info("TEST FUNCTION EXECUTED AFTER: %s", req.method)
+    return func.HttpResponse("HttpTrigger1 function processed a request after deploying via Github Action!")
     
