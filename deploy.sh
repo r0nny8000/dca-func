50c541950b3d2a641b3832bc36add0ab4aba4a18
r0nny8000 <r0nny8000@bea2b.de>
2023-11-08 23:11:14 +0100

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "deploy function in Azure"
+
+
+RESOURCE_GROUP=dcafuncrg
+LOCATION=westeurope
+STORAGE=dcafuncst
+APP=dca-func
+
+az config param-persist off
+
+echo "login..."
+az login
+echo "done."
+
+echo "create a resource group..."
+az group create --name $RESOURCE_GROUP --location $LOCATION
+echo "done."
+
+echo "create a storage..."
+az storage account create --name $STORAGE --location $LOCATION --resource-group $RESOURCE_GROUP --sku Standard_LRS
+echo "done."
+
+echo "create a function..."
+az functionapp create --resource-group $RESOURCE_GROUP --consumption-plan-location $LOCATION --runtime python --runtime-version 3.10 --functions-version 4 --name $APP --os-type linux --storage-account $STORAGE
+echo "done."
+
+echo "publish function..."
+func azure functionapp publish $APP
+echo "done."
+
+echo "config something... :-)"
+az functionapp config appsettings set --name $APP --resource-group $RESOURCE_GROUP --settings AzureWebJobsFeatureFlags=EnableWorkerIndexing
+echo "done."
