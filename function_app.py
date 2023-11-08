f3fe84f7de9e76cb40fd341ce39fbbfabeab1f47
r0nny8000 <r0nny8000@bea2b.de>
2023-11-08 22:03:30 +0100

    added a new fuction within one app for testing only

--- function_app.py
+++ function_app.py
@@ -3,9 +3,15 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.function_name(name="HttpTrigger1")
+@app.function_name(name="Hello")
 @app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
+def hello_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("TEST FUNCTION EXECUTED AFTER: %s", req.method)
-    return func.HttpResponse("HttpTrigger1 function processed a request after deploying via Github Action!")
+    return func.HttpResponse("Hello function processed a request after deploying via Github Action!")
     
+
+@app.function_name(name="World")
+@app.route(route="world", auth_level=func.AuthLevel.ANONYMOUS)
+def hello_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("TEST FUNCTION EXECUTED AFTER: %s", req.method)
+    return func.HttpResponse("World function processed a request after deploying via Github Action!")
