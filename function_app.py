2a934068f65e686e85334cbc647c961b10ac8f6e
r0nny8000 <r0nny8000@bea2b.de>
2023-11-11 14:12:46 +0100

    changed chron to hourly

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ import azure.functions as func
 app = func.FunctionApp()
 
 @app.function_name(name="dcatimer")
-@app.schedule(schedule="*/16 * * * * *", arg_name="dcatimer", run_on_startup=False)
+@app.schedule(schedule="0 0 * * * *", arg_name="dcatimer", run_on_startup=False)
 def dca_timer(dcatimer: func.TimerRequest) -> None:
     logging.info("FUNCTION EXECUTED: %s", datetime.datetime.utcnow())
     
