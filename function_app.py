1d1168c828a6ced65a5fc095371da0bd082cd9f0
r0nny8000 <r0nny8000@bea2b.de>
2023-11-11 17:11:58 +0100

    added http trigger again

--- function_app.py
+++ function_app.py
@@ -4,8 +4,15 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
+@app.function_name(name="dcahttp")
+@app.route(route="dca", auth_level=func.AuthLevel.ANONYMOUS)
+def hello_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("HTTP TRIGGER FUNCTION EXECUTED: %s", req.method)
+    return func.HttpResponse("Hi")
+
+
 @app.function_name(name="dcatimer")
 @app.schedule(schedule="0 0 * * * *", arg_name="dcatimer", run_on_startup=False)
 def dca_timer(dcatimer: func.TimerRequest) -> None:
-    logging.info("FUNCTION EXECUTED: %s", datetime.datetime.utcnow())
+    logging.info("TIMER FUNCTION EXECUTED: %s", datetime.datetime.utcnow())
     
