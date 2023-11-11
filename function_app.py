1a11e07818d64511a1f595163da845e922e8fdbd
r0nny8000 <r0nny8000@bea2b.de>
2023-11-11 12:57:49 +0100

    implemeted timer

--- function_app.py
+++ function_app.py
@@ -1,17 +1,11 @@
+import datetime
 import logging
 import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.function_name(name="Hello")
-@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
-def hello_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("TEST FUNCTION EXECUTED AFTER: %s", req.method)
-    return func.HttpResponse("Hello function processed a request after deploying via Github Action!")
+@app.function_name(name="dcatimer")
+@app.schedule(schedule="00:00:16", arg_name="dcatimer", run_on_startup=False)
+def dca_timer(timer: func.TimerRequest) -> None:
+    logging.info("FUNCTION EXECUTED: %s", datetime.datetime.utcnow())
     
-
-@app.function_name(name="World")
-@app.route(route="world", auth_level=func.AuthLevel.ANONYMOUS)
-def hello_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("TEST FUNCTION EXECUTED AFTER: %s", req.method)
-    return func.HttpResponse("World function processed a request after deploying via Github Action!")
